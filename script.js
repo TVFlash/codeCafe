9355be69982a07b7d6f090aa0a48100b1a91215c
mattmolo <molo.matt@gmail.com>
2015-03-24 14:36:24 -0400

    Added codemirror ide and more updates

---
+++ script.js
@@ -0,0 +1,50 @@
+  var saveTimeout;
+
+  function save() {
+    if (saveTimeout) clearTimeout(saveTimeout);
+    localStorage.setItem("codeCafe_text", myCodeMirror.getValue());
+    $("#save").html("Saved!");
+
+    var saveTimeout = setTimeout(function() {
+      $("#save").html("Save");
+    }, 1000);
+
+  }
+
+  function runData() {
+    save();
+    var result = eval(String(myCodeMirror.getValue()));
+  }
+
+  function Print(input) {
+    var result;
+    var answer = "Hello";
+    if (typeof(input) === 'string') {
+      result = input.trim();
+    } else if(typeof(input) === 'boolean') {
+      console.log("Boolean: "+input)
+      result = input;
+    } else {
+      result = eval(String(input))
+    }
+     
+    $("#rightText").val($("#rightText").val() +"\n"+ ">: "+result);
+    dispRepeat = input;
+    $("#rightText").animate({ scrollTop: "+=20" },1);
+  }
+
+  window.print = Print;
+
+  function switchTheme() {
+    var theme = localStorage.getItem("theme");
+
+    if (theme === "dark") {
+      myCodeMirror.setOption("theme", "xq-light");
+      localStorage.setItem("theme", "light");
+      $("#dark").html("Light");
+    } else {
+      myCodeMirror.setOption("theme", "monokai");
+      localStorage.setItem("theme", "dark");
+      $("#dark").html("Dark");
+    }
+  }
