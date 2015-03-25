608c7e3999619e7b65dbbb5fcebea6ab3a895ab2
Matt Molo <molo.matt@gmail.com>
2015-03-25 05:09:17 -0400

    Lots of updates
    Font increase and decrease, individual clear buttons, bracket
    highlighting, and more I think!

--- script.js
+++ script.js
@@ -1,61 +1,95 @@
-  var saveTimeout;
+	var saveTimeout;
 
-  function save() {
-    if (saveTimeout) clearTimeout(saveTimeout);
-    localStorage.setItem("codeCafe_text", myCodeMirror.getValue());
-    $("#save").html("Saved!");
+	function save() {
+		if (saveTimeout) clearTimeout(saveTimeout);
+    
+		localStorage.setItem("codeCafe_text", myCodeMirror.getValue());
+		$("#save").html("Saved!");
 
-    var saveTimeout = setTimeout(function() {
-      $("#save").html("Save");
-    }, 1000);
+		var saveTimeout = setTimeout(function() {
+			$("#save").html("Save");
+		}, 1000);
+	
+		myCodeMirror.focus();
+	}
 
-  }
+	function runData() {
+		save();
+		try {
+			var result = eval(String(myCodeMirror.getValue()));
+		} catch (e) {
+			console.log(e);
+			result = e.message;
+			$("#rightText").val($("#rightText").val() +"\n"+ ">: "+result);
+		}
+	}
 
-  function runData() {
-    save();
-            try {
-                var result = eval(String(myCodeMirror.getValue()));
-            } catch (e) {
-                console.log(e);
-                result = e.message;
-    $("#rightText").val($("#rightText").val() +"\n"+ ">: "+result);
+	function clearIDE() {
+		myCodeMirror.setValue("");
+		
+		myCodeMirror.focus();
+	}
+	
+	function clearTerminal() {
+		$("#rightText").val("");
+		
+		myCodeMirror.focus();
+	}
 
-            }
-  }
-
-  function clearS() {
-        $("#rightText").val("");
-  }
-
-  function Print(input) {
-    var result;
-    var answer = "Hello";
-    if (typeof(input) === 'string') {
-      result = input.trim();
-    } else if(typeof(input) === 'boolean') {
-      console.log("Boolean: "+input)
-      result = input;
-    } else {
-      result = eval(String(input))
-    }
+	function Print(input) {
+		var result;
+		var answer = "Hello";
+	
+		if (typeof(input) === 'string') {
+			result = input.trim();
+		} else if(typeof(input) === 'boolean') {
+			console.log("Boolean: "+input)
+			result = input;
+		} else {
+			result = eval(String(input))
+		}
      
-    $("#rightText").val($("#rightText").val() +"\n"+ ">: "+result);
-    dispRepeat = input;
-    $("#rightText").animate({ scrollTop: "+=20" },1);
-  }
+		$("#rightText").val($("#rightText").val() +"\n"+ ">: "+result);
+		dispRepeat = input;
+		$("#rightText").animate({ scrollTop: "+=200"},1);
+	}
 
-  window.print = Print;
-  window.clear = clearS;
-  function switchTheme() {
-    var theme = localStorage.getItem("theme");
+	function switchTheme() {
+		var theme = localStorage.getItem("theme");
 
-    if (theme === "dark") {
-      myCodeMirror.setOption("theme", "xq-light");
-      localStorage.setItem("theme", "light");
-      $("#dark").html("Light");
-    } else {
-      myCodeMirror.setOption("theme", "monokai");
-      localStorage.setItem("theme", "dark");
-      $("#dark").html("Dark");
-    }
-  }
+		if (theme === "dark") {
+			myCodeMirror.setOption("theme", "xq-light");
+			localStorage.setItem("theme", "light");
+			$("#dark").html("Dark");
+		} else {
+			myCodeMirror.setOption("theme", "monokai");
+			localStorage.setItem("theme", "dark");
+			$("#dark").html("Light");
+		}
+		
+		myCodeMirror.focus();
+	}
+	
+	function fontInc() {
+		var fontSize = parseInt(localStorage.getItem("fontSize"));
+		if (fontSize >= 56) return;
+		fontSize = fontSize+2;
+		$('.CodeMirror').css("font-size", fontSize+"px");
+		$('#rightText').css("font-size", fontSize+"px");
+		localStorage.setItem("fontSize", fontSize);
+		myCodeMirror.refresh();
+		myCodeMirror.focus();
+	}
+	
+	function fontDec() {
+		var fontSize = parseInt(localStorage.getItem("fontSize"));
+		if (fontSize <= 2) return;
+		fontSize = fontSize-2;
+		$('.CodeMirror').css("font-size", fontSize+"px");
+		$('#rightText').css("font-size", fontSize+"px");
+		localStorage.setItem("fontSize", fontSize);	
+		myCodeMirror.refresh();
+		myCodeMirror.focus();
+	}
+  
+	window.print = Print;
